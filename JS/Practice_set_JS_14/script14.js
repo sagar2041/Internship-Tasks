39547bd18038507bd1fa9da65be4c100052a49db
sagar2041 <patilsagar1722@gmail.com>
2024-03-06 12:33:05 +0530

    complete 14 task

---
+++ JS/Practice_set_JS_14/script14.js
@@ -0,0 +1,17 @@
+// Append text
+
+let heading = document.getElementById("heading");
+let btn = document.getElementById("btn");
+btn.addEventListener("click",()=>{
+    heading.innerHTML = "Parsh infotech students!";
+})
+
+// divs task
+let btn1 = document.getElementById("btn1")
+let boxInfo = document.querySelectorAll(".box")
+
+btn1.addEventListener("click",()=>{
+    boxInfo[0].innerHTML = "Hello Javascript";
+    boxInfo[1].innerHTML = "Paarsh Infotech";
+    boxInfo[2].innerHTML = "Apna College";
+})
